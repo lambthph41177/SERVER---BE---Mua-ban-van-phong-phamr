14ad7922218f912eb7084fd35cebb53a28e63189
Dinh-Hoai-Nam <dinhhoainam20304@gmail.com>
2026-04-11 14:30:53 +0700

    "Tạo model User: - Khai báo schema người dùng với username, email, password - Phân quyền role (admin, user, manage) - Bổ sung avatar, địa chỉ, số điện thoại và trạng thái active - Hỗ trợ reset mật khẩu và bắt buộc đổi mật khẩu - Thêm timestamps tự động tạo createdAt, updatedAt"

---
+++ src/model/User.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+const userSchma = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user", "manage"],
+      default: "admin",
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://i.pinimg.com/736x/bc/43/98/bc439871417621836a0eeea768d60944.jpg",
+    },
+    address: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    resetPasswordToken: {
+      type: String,
+    },
+    resetPasswordExpire: {
+      type: Date,
+    },
+    mustChangePassword: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.models.Users || mongoose.model("Users", userSchma);
