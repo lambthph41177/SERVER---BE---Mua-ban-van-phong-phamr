097bc5e612cbb8f70d97f677282399b96a17f471
HoangHuan218 <166848474+HoangHuan218@users.noreply.github.com>
2026-05-04 13:53:10 +0000

    DONE: fix cart thêm timestamps theo dõi ngày cập nhật giỏ hàng

--- src/model/cart.js
+++ src/model/cart.js
@@ -1,21 +1,24 @@
 import mongoose from "mongoose";
 
-const cartSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-    required: true,
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "products",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
   },
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "products",
-    required: true,
-  },
-  quantity: {
-    required: true,
-    type: Number,
-    default: 1,
-  },
-});
+  { timestamps: true } // Thêm cái này để theo dõi ngày tạo/cập nhật giỏ hàng
+);
 
 export const Cart = mongoose.models.carts || mongoose.model("carts", cartSchema);
